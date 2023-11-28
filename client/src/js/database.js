373222accb0bae3df8a7679272b86cf60f4a09a2
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-11-28 13:06:49 -0600

    finished the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,31 +3,48 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () =>
-  openDB('todos', 1, {
+  openDB('info', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('todos')) {
-        console.log('todos database already exists');
+      if (db.objectStoreNames.contains('info')) {
+        console.log('info database already exists');
         return;
       }
-      db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
-      console.log('todos database created');
+      db.createObjectStore('info', { keyPath: 'id', autoIncrement: true });
+      console.log('info database created');
     },
   });
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+ const info = await openDB('info', 1);
+ const tx = info.transaction('info', 'readwrite');
+ const store = tx.objectStore('info');
+ const request = store.add({ name, home, cell, email });
+ const result = await request;
+ console.log("data has been saved", result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+  const info = await openDB('info', 1);
+  const tx = info.transaction('info', 'readonly');
+  const store = tx.objectStore('info');
+  const request = store.getAll();
+  const result = await request;
+  console.log("data has been retrieved", result);
+  return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  const info = await openDB('info', 1);
+  const tx = info.transaction('info', 'readwrite');
+  const store = tx.objectStore('info');
+  const request = store.delete(id);
+  const result = await request;
+  console.log("data has been deleted", result);
+  return result;
 };
 
 initdb();
