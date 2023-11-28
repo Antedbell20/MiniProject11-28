bf35c3875ebcc2335df7045f2674b2c296647ce0
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-11-28 13:27:32 -0600

    sw.js stuff

---
+++ client/src/sw.js
@@ -0,0 +1,41 @@
+// TODO: Create a service worker that caches static assets:
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+import {ChacheableResponsePlugin} from 'workbox-cacheable-response';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    ({request}) => {
+        return(
+            request.destination === 'style' ||
+            request.destination === 'script'
+        );
+    },
+    new StaleWhileRevalidate({
+        cacheName: 'static-resources',
+        plugins:[
+            new ChacheableResponsePlugin({
+                statuses [0, 200],
+            }),
+        ],
+    })
+);
+
+registerRoute(
+    ({request}) => request.destination === 'image',
+    new CacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new ChacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+            }),
+        ],
+    })
+);
